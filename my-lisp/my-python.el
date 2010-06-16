7c8ed632ea1333014c95e093eaafed5bbb3d84c6
phil-s <github@trashmail.net>
2010-06-16 15:24:47 +1200

    Commented out the non-working pyflakes support.

--- my-lisp/my-python.el
+++ my-lisp/my-python.el
@@ -17,20 +17,32 @@
 (defun my-python-mode-hook ()
   (hide-trailing-whitespace)
   (setq indent-tabs-mode nil)
-  (make-local-variable 'find-file-hook)
-  (add-hook 'find-file-hook 'flymake-find-file-hook))
 
-;; Flymake with pyflakes
-(eval-after-load "flymake"
-  (defun flymake-pyflakes-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list "pyflakes" (list local-file))))
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pyflakes-init)))
+  ;; (require 'flymake)
+
+  ;; (make-local-variable 'find-file-hook)
+  ;; (add-hook 'find-file-hook 'flymake-find-file-hook)
+
+  ;; (make-local-variable 'flymake-allowed-file-name-masks)
+  ;; (if (boundp 'flymake-allowed-file-name-masks)
+  ;;     (add-to-list 'flymake-allowed-file-name-masks
+  ;;                  '("\\.py\\'" flymake-pyflakes-init))
+  ;;   (setq flymake-allowed-file-name-masks
+  ;;         '("\\.py\\'" flymake-pyflakes-init)))
+  )
+
+;; ;; Flymake with pyflakes
+;; (eval-after-load "flymake"
+;;   (progn
+;;     (message "Hello, world... boo!")
+;;     (defun flymake-pyflakes-init ()
+;;       (let* ((temp-file (flymake-init-create-temp-buffer-copy
+;;                          'flymake-create-temp-inplace))
+;;              (local-file (file-relative-name
+;;                           temp-file
+;;                           (file-name-directory buffer-file-name))))
+;;         (list "pyflakes" (list local-file))))
+;;     ))
 
 ;;;; ipython
 
