6739c25ffe4be0fa02a91bcb36a43f5ac533a74b
phil-s <github@trashmail.net>
2010-07-16 00:02:09 +1200

    elisp tweaks.

--- my-lisp/my-programming.el
+++ my-lisp/my-programming.el
@@ -162,12 +162,12 @@ context-help to false"
   (match-string 0))
 
 
-;; HTML / ASP / VBSCRIPT
-(setq auto-mode-alist (cons '("\\.html$" . html-helper-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.asp$" . html-helper-mode) auto-mode-alist))
-(add-hook 'html-helper-load-hook '(lambda () (require 'visual-basic-mode)))
-(add-hook 'html-helper-load-hook '(lambda () (require 'html-font)))
-(add-hook 'html-helper-mode-hook '(lambda () (font-lock-mode 1)))
+;; HTML ;;/ ASP / VBSCRIPT
+(add-to-list 'auto-mode-alist '("\\.html\\'" . html-helper-mode))
+;;(add-to-list 'auto-mode-alist '("\\.html\\'" . html-helper-mode))
+;;(add-hook 'html-helper-load-hook (function (lambda () (require 'visual-basic-mode))))
+(add-hook 'html-helper-load-hook (function (lambda () (require 'html-font))))
+(add-hook 'html-helper-mode-hook (function (lambda () (font-lock-mode 1))))
 
 
 ;; Wrap-region minor mode for mark-up
--- my-lisp/my-python.el
+++ my-lisp/my-python.el
@@ -16,7 +16,7 @@
 
 
 ;;;; python-mode.el
-(setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (setq interpreter-mode-alist (cons '("python" . python-mode)
                                    interpreter-mode-alist))
 (autoload 'python-mode "python-mode" "Python editing mode." t)
