4435fc57d7f6cd689611e02826b99a0695be1d12
phil-s <github@trashmail.net>
2010-08-20 14:58:00 +1200

    init.el accompanying previous changeset.

--- init.el
+++ init.el
@@ -84,7 +84,7 @@
  '(dnd-protocol-alist (quote (("^file:///" . dnd-open-local-file) ("^file://" . dnd-open-file) ("^file:[A-Za-z]%3a" . dnd-open-local-file-fix-url) ("^file:" . dnd-open-local-file) ("^\\(https?\\|ftp\\|file\\|nfs\\)://" . dnd-open-file))))
  '(global-font-lock-mode t nil (font-lock))
  '(history-length 100)
- '(ibuffer-formats (quote ((mark modified read-only " " (name 30 30 :left :elide) " " (size 9 -1 :right) " " (mode 16 16 :left :elide) " " filename-and-process) (mark " " (name 16 -1) " " filename))))
+ '(ibuffer-formats (quote ((mark modified read-only " " (name 30 60 :left :elide) " " (size 9 -1 :right) " " (mode 16 16 :left :elide) " " filename-and-process) (mark " " (name 16 -1) " " filename))))
  '(ibuffer-saved-filters (quote (("gnus" ((or (mode . message-mode) (mode . mail-mode) (mode . gnus-group-mode) (mode . gnus-summary-mode) (mode . gnus-article-mode)))) ("programming" ((or (mode . emacs-lisp-mode) (mode . cperl-mode) (mode . c-mode) (mode . java-mode) (mode . idl-mode) (mode . lisp-mode)))))))
  '(inhibit-eol-conversion nil)
  '(read-buffer-completion-ignore-case t)
@@ -98,7 +98,8 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- )
+ '(whitespace-newline ((t (:foreground "grey32" :weight normal))))
+ '(whitespace-space ((((class color) (background dark)) (:foreground "grey30")))))
 ;; Warning: Under Win32 (NTEmacs), my-theme.el over-rides custom-set-faces
 ;; for the 'user theme, to set the default font face. Custom faces set
 ;; in the above call will be over-ridden in Win32.
@@ -133,6 +134,9 @@
 ;; Configure visual theme
 (require 'my-theme)
 
+;; Session management
+(require 'my-session)
+
 ;; Support for development on local machine
 (require 'my-local)
 
@@ -151,3 +155,55 @@
 (global-set-key (kbd "M-s /")   'my-multi-occur-in-matching-buffers)
 (global-set-key (kbd "C-c i")   'imenu-ido-goto-symbol)
 (global-set-key (kbd "C-c c")   'clone-line)
+(global-set-key (kbd "C-h C-f") 'find-function)
+
+
+
+
+;; Determining running environment and platform capabilities in Emacs.
+;; http://brain-break.blogspot.com/2010/08/determining-running-environment-and.html
+
+;;;; Check variables:
+;;;;
+;;emacs-major-version
+;;emacs-minor-version
+;;window-system            ;'nil' if in terminal, 'w32' if native Windows build,
+;;                         ;'x' if under X Window
+;;window-system-version    ;for windows only
+;;operating-system-release ;release of the operating system Emacs is running on
+;;system-configuration     ;like configuration triplet: cpu-manufacturer-os
+;;system-name              ;host name of the machine you are running on
+;;system-time-locale
+;;system-type              ;indicating the type of operating system:
+;;                         ;'gnu' (GNU Hurd), 'gnu/linux', 'gnu/kfreebsd'
+;;                         ;(FreeBSD), 'darwin' (GNU-Darwin, Mac OS X),
+;;                         ;'ms-dos', 'windows-nt', 'cygwin'
+;;system-uses-terminfo
+;;window-size-fixed
+
+;;;; Check functions:
+;;;;
+;;(fboundp ...)            ;return t if SYMBOL's function definition is not void
+;;(featurep ...)           ;returns t if FEATURE is present in this Emacs
+;;(display-graphic-p)      ;return non-nil if DISPLAY is a graphic display;
+;;                         ;graphical displays are those which are capable of
+;;                         ;displaying several frames and several different
+;;                         ;fonts at once
+;;(display-multi-font-p)   ;same as 'display-graphic-p'
+;;(display-multi-frame-p)  ;same as 'display-graphic-p'
+;;(display-color-p)        ;return t if DISPLAY supports color
+;;(display-images-p)       ;return non-nil if DISPLAY can display images
+;;(display-grayscale-p)    ;return non-nil if frames on DISPLAY can display
+;;                         ;shades of gray
+;;(display-mouse-p)        ;return non-nil if DISPLAY has a mouse available
+;;(display-popup-menus-p)  ;return non-nil if popup menus are supported on
+;;                         ;DISPLAY
+;;(display-selections-p)   ;return non-nil if DISPLAY supports selections
+
+;;;; Run those checks as below:
+;;;;
+;;(when window-system ...)
+;;(when (eq window-system 'x) ...)
+;;(when (>= emacs-major-version 22) ...)
+;;(when (fboundp '...) ...)
+;;(when (featurep '...) ...)
