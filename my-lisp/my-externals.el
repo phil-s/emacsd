a49061f646c80762bc8e40210a9aa45a48886209
phil-s <github@trashmail.net>
2010-10-18 14:37:16 +1300

    Added el-get external library management.

---
+++ my-lisp/my-externals.el
@@ -0,0 +1,62 @@
+(setq
+ el-get-sources
+ '(
+   (:name el-get
+          :type git
+          :url "git://github.com/dimitri/el-get.git")
+
+   (:name ws-trim
+          :type http
+          :url "ftp://ftp.lysator.liu.se/pub/emacs/ws-trim.el")
+
+   ;; (:name browse-kill-ring
+   ;;        :type http
+   ;;        :url "http://www.emacswiki.org/emacs/download/browse-kill-ring.el"
+   ;;        :features browse-kill-ring)
+
+   ;; (:name outline-copy-visible
+   ;;        :type http
+   ;;        :url "http://gist.github.com/raw/519635/yank-visible.el"
+   ;;        :loads "outline-copy-visible")
+
+   ;; (:name scratch
+   ;;        :type http
+   ;;        ;;:url "git@github.com:cbbrowne/scratch-el.git/master/scratch.el")
+   ;;        :url "http://github.com/ieure/scratch-el/raw/master/scratch.el"
+   ;;        :features scratch)
+
+   ;; ;; (:name color-theme
+   ;; ;;        :type http
+   ;; ;;        :url "http://download.savannah.nongnu.org/releases/color-theme/"
+   ;; ;;        :features color-theme)
+
+   ;; ;; (:name magit
+   ;; ;;        :type git
+   ;; ;;        :url "http://github.com/philjackson/magit.git"
+   ;; ;;        :info "."
+   ;; ;;        :build ("./autogen.sh" "./configure" "make"))
+
+   ;; ;; (:name yasnippet
+   ;; ;;        :type git-svn
+   ;; ;;        :url "http://yasnippet.googlecode.com/svn/trunk/")
+
+   ;; ;; (:name asciidoc         :type elpa)
+   ;; ;; (:name dictionary-el    :type apt-get)
+   ))
+
+;;;;(require 'el-get)
+(if (not (functionp 'el-get))
+    (let ((el-get
+           (expand-file-name (concat
+                              user-emacs-directory
+                              "el-get/el-get/el-get.el"))))
+      (if (file-exists-p el-get)
+	  (load el-get))))
+
+;; Execute
+(if (functionp 'el-get)
+    (el-get))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(provide 'my-externals)
