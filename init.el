f85b06ac027887dbcfc814f61d3da1ab5d418cec
phil-s <github@trashmail.net>
2010-10-18 14:59:30 +1300

    Default font.

--- init.el
+++ init.el
@@ -107,6 +107,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#3f3f3f" :foreground "#dcdccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 121 :width normal :foundry "unknown" :family "WenQuanYi Micro Hei Mono"))))
  '(whitespace-newline ((t (:foreground "grey32" :weight normal))))
  '(whitespace-space ((((class color) (background dark)) (:foreground "grey30")))))
 ;; Warning: Under Win32 (NTEmacs), my-theme.el over-rides custom-set-faces
