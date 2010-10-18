b8cfa4e5bea5b71baeb901ad1b68b8d2b56cba9e
phil-s <github@trashmail.net>
2010-10-18 14:53:51 +1300

    Load my-desktop in one step.

--- my-lisp/my-session.el
+++ my-lisp/my-session.el
@@ -36,6 +36,13 @@
       '(kill-ring))
 (savehist-mode 1)
 
+(defun my-desktop ()
+  "Load the desktop and enable autosaving"
+  (interactive)
+  (let ((desktop-load-locked-desktop "ask"))
+    (desktop-read)
+    (desktop-save-mode 1)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (provide 'my-session)
