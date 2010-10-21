492af93de37e3a1c08fe9a9f8bcd27fc29559a42
phil-s <github@trashmail.net>
2010-10-22 11:47:38 +1300

    Adding el-get config.

---
+++ el-get/color-theme/color-theme.el
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<html>
+ <head>
+  <title>Index of /Unix/NonGNU/color-theme</title>
+ </head>
+ <body>
+<h1>Index of /Unix/NonGNU/color-theme</h1>
+<table><tr><th><img src="/icons/blank.gif" alt="[ICO]"></th><th><a href="?C=N;O=D">Name</a></th><th><a href="?C=M;O=A">Last modified</a></th><th><a href="?C=S;O=A">Size</a></th><th><a href="?C=D;O=A">Description</a></th></tr><tr><th colspan="5"><hr></th></tr>
+<tr><td valign="top"><img src="/icons/back.gif" alt="[DIR]"></td><td><a href="/Unix/NonGNU/">Parent Directory</a>       </td><td>&nbsp;</td><td align="right">  - </td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/folder.gif" alt="[DIR]"></td><td><a href="6.5.5/">6.5.5/</a>                 </td><td align="right">13-Apr-2009 04:02  </td><td align="right">  - </td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/compressed.gif" alt="[   ]"></td><td><a href="color-theme-6.6.0.tar.gz">color-theme-6.6.0.ta..&gt;</a></td><td align="right">13-Apr-2009 04:02  </td><td align="right">122K</td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td><td><a href="color-theme-6.6.0.tar.gz.sig">color-theme-6.6.0.ta..&gt;</a></td><td align="right">13-Apr-2009 04:02  </td><td align="right"> 65 </td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/compressed.gif" alt="[   ]"></td><td><a href="color-theme-6.6.0.zip">color-theme-6.6.0.zip</a>  </td><td align="right">13-Apr-2009 04:02  </td><td align="right">131K</td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td><td><a href="color-theme-6.6.0.zip.sig">color-theme-6.6.0.zi..&gt;</a></td><td align="right">13-Apr-2009 04:02  </td><td align="right"> 65 </td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/compressed.gif" alt="[   ]"></td><td><a href="color-theme.el.gz">color-theme.el.gz</a>      </td><td align="right">13-Apr-2009 04:02  </td><td align="right">110K</td><td>&nbsp;</td></tr>
+<tr><th colspan="5"><hr></th></tr>
+</table>
+</body></html>
