046e72975d99d80767e06277fe5b43f8f3a08079
phil-s <github@trashmail.net>
2010-10-23 00:51:39 +1300

    ws-trim via ftp not behaving for el-get.

--- my-lisp/my-externals.el
+++ my-lisp/my-externals.el
@@ -32,9 +32,9 @@
           :url "http://www.emacswiki.org/emacs/download/fic-mode.el"
           :features fic-mode)
 
-   (:name ws-trim
-          :type http
-          :url "ftp://ftp.lysator.liu.se/pub/emacs/ws-trim.el")
+   ;; (:name ws-trim
+   ;;        :type http
+   ;;        :url "ftp://ftp.lysator.liu.se/pub/emacs/ws-trim.el")
 
    ;; (:name color-theme
    ;;        :type http
@@ -55,6 +55,35 @@
    ;; (:name dictionary-el    :type apt-get)
    ))
 
+
+(defun install-el-get ()
+  "So the idea is that you copy/paste this code into your *scratch* buffer, hit C-j, and you have a working el-get."
+  (let* ((el-get-dir        (expand-file-name "~/.emacs.d/el-get/"))
+         (dummy             (unless (file-directory-p el-get-dir)
+                              (make-directory el-get-dir t)))
+         (package           "el-get")
+         (bname             "*el-get bootstrap*") ; both process and buffer name
+         (pdir              (concat (file-name-as-directory el-get-dir) package))
+         (git               (or (executable-find "git") (error "Unable to find `git'")))
+         (url               "git://github.com/dimitri/el-get.git")
+         (el-get-sources    `((:name ,package :type "git" :url ,url :features el-get :compile "el-get.el")))
+         (default-directory el-get-dir)
+         (process-connection-type nil) ; pipe, no pty (--no-progress)
+         (clone             (start-process bname bname git "--no-pager" "clone" "-v" url package)))
+    (set-window-buffer (selected-window) (process-buffer clone))
+    (set-process-sentinel
+     clone
+     `(lambda (proc change)
+        (when (eq (process-status proc) 'exit)
+          (setq default-directory (file-name-as-directory ,pdir))
+          (setq el-get-sources ',el-get-sources)
+          (load (concat (file-name-as-directory ,pdir) ,package ".el"))
+          (el-get-init "el-get")
+          (with-current-buffer (process-buffer proc)
+            (goto-char (point-max))
+            (insert "\nCongrats, el-get is installed and ready to serve!"))))))
+  )
+
 ;;;;(require 'el-get)
 (if (not (functionp 'el-get))
     (let ((el-get
@@ -62,9 +91,12 @@
                               user-emacs-directory
                               "el-get/el-get/el-get.el"))))
       (if (file-exists-p el-get)
-	  (load el-get))))
+          (load el-get))))
 
 ;; Execute
+(if (not (functionp 'el-get))
+    (install-el-get))
+
 (if (functionp 'el-get)
     (el-get))
 
