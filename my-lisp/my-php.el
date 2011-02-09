2de8412bf9ced89129662a66c0151f1d8d54f625
phil-s <github@trashmail.net>
2011-02-09 13:59:32 +1300

    Drupal debugging aid.

--- my-lisp/my-php.el
+++ my-lisp/my-php.el
@@ -77,6 +77,7 @@
   (interactive)
   (php-mode)
   (my--drupal-mode)
+  (local-set-key (kbd "C-c q") 'drupal-quick-and-dirty-debugging)
   (message "Drupal mode activated."))
 
 (defun my--drupal-mode ()
@@ -92,3 +93,5 @@
   (setq paragraph-start (concat paragraph-start "\\| \\* @[a-z]+")
         paragraph-separate "$"))
 
+(fset 'drupal-quick-and-dirty-debugging
+   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([C-home 19 102 117 110 99 116 105 111 110 32 100 114 117 112 97 108 95 115 101 116 95 109 101 115 115 97 103 101 5 return tab 105 102 32 40 36 116 121 112 101 32 61 61 32 39 101 114 114 111 114 39 41 32 123 return tab 100 115 109 40 100 101 98 117 103 95 98 97 99 107 116 114 97 99 101 40 41 44 32 84 82 85 69 41 59 return tab 125 tab] 0 "%d")) arg)))
