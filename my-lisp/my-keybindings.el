6d986a24b2b4e60bcbd4762dbcf930aca0ee00ad
phil-s <github@trashmail.net>
2011-08-05 01:52:10 +1200

    Various keybinding fixes.

--- my-lisp/my-keybindings.el
+++ my-lisp/my-keybindings.el
@@ -1,10 +1,11 @@
 ;; Custom 'apropos' key bindings
-(define-prefix-command 'Apropos-Prefix nil "Apropos (a,d,f,l,v,C-v)")
+(define-prefix-command 'Apropos-Prefix nil "Apropos (a,d,f,i,l,v,C-v)")
 (define-key Apropos-Prefix (kbd "a")   'apropos)
 (define-key Apropos-Prefix (kbd "C-a") 'apropos)
 (define-key Apropos-Prefix (kbd "d")   'apropos-documentation)
 (define-key Apropos-Prefix (kbd "f")   'apropos-command)
 (define-key Apropos-Prefix (kbd "c")   'apropos-command)
+(define-key Apropos-Prefix (kbd "i")   'info-apropos)
 (define-key Apropos-Prefix (kbd "l")   'apropos-library)
 (define-key Apropos-Prefix (kbd "v")   'apropos-variable)
 (define-key Apropos-Prefix (kbd "C-v") 'apropos-value)
@@ -23,9 +24,6 @@
 
   ;; Use ibuffer in place of list-buffers
   (define-key keymap (kbd "C-x C-b")   'ibuffer)
-
-  ;; Use hippie-expand in place of dabbrev-expand
-  (define-key keymap (kbd "M-/")       'hippie-expand)
 
   ;; Whitespace
   (define-key keymap (kbd "C-x M-w")   'toggle-show-trailing-whitespace)
@@ -52,7 +50,7 @@
   (define-key keymap (kbd "C-c <C-left>") 'winner-undo)
   (define-key keymap (kbd "C-c <C-right>") 'winner-redo)
 
-  ;; Replicate windmove bindings, so that they don't get clobbered
+  ;; Replicate windmove bindings, so that they can't get clobbered
   (define-key keymap (kbd "<S-up>")    'windmove-up)
   (define-key keymap (kbd "<S-down>")  'windmove-down)
   (define-key keymap (kbd "<S-left>")  'windmove-left)
@@ -62,6 +60,7 @@
   (define-key keymap (kbd "C-M-=")     'compare-windows)
 
   ;; Completions
+  (define-key keymap (kbd "M-/")       'hippie-expand) ; In place of dabbrev-expand
   (define-key keymap (kbd "C-c /")     'my-ido-hippie-expand)
 
   ;; Miscellaneous (mine/third-party)
@@ -89,22 +88,41 @@
   (define-key keymap (kbd "C-h C-f")   'find-function)
   (define-key keymap (kbd "C-h C-v")   'find-variable)
   (define-key keymap (kbd "C-x C-j")   'dired-jump)
-
-  ;; Make emacs consistent with xkcd :)
-  (define-key keymap (kbd "C-x M-c M-b u t t e r f l y") 'butterfly)
   )
+
+;; Make emacs consistent with xkcd :)
+;; (Too many inferred prefixes to put this in the minor mode
+;; key map, as the map is printed with the documentation.)
+(global-set-key (kbd "C-x M-c M-b u t t e r f l y") 'butterfly)
 
 (defun my-keybindings-after-init-hook ()
   "Define and enable our minor mode after the init file has been loaded.
 We want this to be our final initialisation step, to ensure that
-`my-keys-minor-mode' is first in `minor-mode-map-alist', and therefore
+my-keys-minor-mode is first in `minor-mode-map-alist', and therefore
 takes precedence over other minor mode keymaps.
 
-We also advise `load' to try to retain this priority, subsequent to the
-future definition of other minor modes."
+We also advise `load', to try to retain this priority subsequent
+to the loading of other minor modes.
+
+An alternative would be to use `emulation-mode-map-alists', which has
+a higher priority than minor-mode-map-alist:
+
+This variable holds a list of keymap alists to use for emulations
+modes. It is intended for modes or packages using multiple
+minor-mode keymaps. Each element is a keymap alist which has the
+same format and meaning as minor-mode-map-alist, or a symbol with
+a variable binding which is such an alist. The 'active' keymaps
+in each alist are used before minor-mode-map-alist and
+minor-mode-overriding-map-alist."
 
   (define-minor-mode my-keys-minor-mode
     "A minor mode so that my custom key bindings take precedence over major modes.
+
+We also have precedence over minor mode keymaps which appear later in the
+`minor-mode-map-alist' variable, however `emulation-mode-map-alists' keymaps
+will over-ride us.
+
+TODO: Switch to using emulation-mode-map-alists
 
 \\{my-keys-minor-mode-map}"
     :init-value t
