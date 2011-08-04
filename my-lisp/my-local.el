31f1e1c13f1bcc49756a8b4611027d354d3415a9
phil-s <github@trashmail.net>
2011-08-05 02:43:03 +1200

    Added documentation to my-local.el re: git update-index --assume-unchanged

--- my-lisp/my-local.el
+++ my-lisp/my-local.el
@@ -1,3 +1,16 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (provide 'my-local)
+
+;; Adding tracked files to .gitignore won't work because GIT will
+;; still track the changes and commit the file if you use the -a
+;; parameter.
+
+;; To ignore changes to a tracked file:
+;; git update-index --assume-unchanged <file>
+
+;; To start tracking changes again:
+;; git update-index --no-assume-unchanged <file>
+
+;; See:
+;; http://www.kernel.org/pub/software/scm/git/docs/git-update-index.html
