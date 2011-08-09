3ad58e35b6ab233dcf759c3e5fe4fed41eaa22b5
phil-s <github@trashmail.net>
2011-08-09 23:45:01 +1200

    Changing else-less ifs to whens.

--- my-lisp/my-keybindings.el
+++ my-lisp/my-keybindings.el
@@ -141,10 +141,10 @@ TODO: Switch to using emulation-mode-map-alists
 
   (defadvice load (after give-my-keybindings-priority)
     "Try to ensure that my keybindings always have priority."
-    (if (not (eq (car (car minor-mode-map-alist)) 'my-keys-minor-mode))
-        (let ((mykeys (assq 'my-keys-minor-mode minor-mode-map-alist)))
-          (assq-delete-all 'my-keys-minor-mode minor-mode-map-alist)
-          (add-to-list 'minor-mode-map-alist mykeys))))
+    (when (not (eq (car (car minor-mode-map-alist)) 'my-keys-minor-mode))
+      (let ((mykeys (assq 'my-keys-minor-mode minor-mode-map-alist)))
+        (assq-delete-all 'my-keys-minor-mode minor-mode-map-alist)
+        (add-to-list 'minor-mode-map-alist mykeys))))
   (ad-activate 'load))
 
 (add-hook 'after-init-hook 'my-keybindings-after-init-hook)
