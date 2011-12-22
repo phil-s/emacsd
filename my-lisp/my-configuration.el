76c0e617c5723248a96697e65262c3ded6d6848c
Phil Sainty <phil@catalyst.net.nz>
2011-12-23 12:32:24 +1300

    Enable narrow-to-page binding ("C-x n p")

--- my-lisp/my-configuration.el
+++ my-lisp/my-configuration.el
@@ -19,6 +19,7 @@
 ;; Enable disabled commands
 (put 'dired-find-alternate-file 'disabled nil)
 (put 'downcase-region           'disabled nil)
+(put 'narrow-to-page            'disabled nil)
 (put 'narrow-to-region          'disabled nil)
 (put 'scroll-left               'disabled nil)
 (put 'upcase-region             'disabled nil)
