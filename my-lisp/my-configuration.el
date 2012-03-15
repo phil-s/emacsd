c00257bd3699a44defa64af2d63d55edfa7f3ebb
phil-s <github@trashmail.net>
2012-03-16 00:42:37 +1300

    Made linum-mode's format calculations more efficient.

--- my-lisp/my-configuration.el
+++ my-lisp/my-configuration.el
@@ -471,6 +471,22 @@ return to the save-some-buffers minibuffer prompt."
 
 (ad-activate 'ask-user-about-supersession-threat)
 
+;; Make linum's format calculation more efficient
+(defvar my-linum-format-string "%4d")
+
+(add-hook 'linum-before-numbering-hook 'my-linum-get-format-string)
+
+(defun my-linum-get-format-string ()
+  (let* ((width (length (number-to-string
+                         (count-lines (point-min) (point-max)))))
+         (format (concat "%" (number-to-string width) "d")))
+    (setq my-linum-format-string format)))
+
+(setq linum-format 'my-linum-format)
+
+(defun my-linum-format (line-number)
+  (propertize (format my-linum-format-string line-number) 'face 'linum))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (provide 'my-configuration)
