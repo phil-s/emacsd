65c80eb6debc2f4af9f9c4f4827e334281bb7b33
Phil Sainty <phil@catalyst.net.nz>
2012-04-23 19:21:22 +1200

    Added my-split-window-below (C-x 2)

--- my-lisp/my-keybindings.el
+++ my-lisp/my-keybindings.el
@@ -65,6 +65,7 @@
   (define-key keymap (kbd "C-x M-2")   'split-window-vertically-change-buffer)
   (define-key keymap (kbd "C-c \\")    'toggle-window-split)
   (define-key keymap (kbd "C-c C-\\")  'transpose-frame)
+  (define-key keymap (kbd "C-x 2")     'my-split-window-below)
 
   ;; compare-windows
   (define-key keymap (kbd "C-M-=")     'compare-windows)
