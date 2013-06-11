c10d2f7cb0b16556492401cab10e74763a6cc253
Phil <phil@xerxes.(none)>
2013-06-11 21:42:45 +1200

    Add a non-regexp `dired-do-query-replace-regexp' equivalent.

--- my-lisp/my-utilities.el
+++ my-lisp/my-utilities.el
@@ -492,6 +492,55 @@ within the current buffer-file-name."
        (let* ((fn-list (dired-get-marked-files nil arg)))
          (mapc 'find-file fn-list)))))
 
+;; Add a non-regexp `dired-do-query-replace-regexp' equivalent.
+(eval-after-load 'dired
+  '(define-key dired-mode-map (kbd "C-c Q") 'my-dired-do-query-replace))
+
+(defun my-dired-do-query-replace (from to &optional delimited)
+  "Do `query-replace' of FROM with TO, on all marked files.
+Third arg DELIMITED (prefix arg) means replace only word-delimited matches.
+If you exit (\\[keyboard-quit], RET or q), you can resume the query replace
+with the command \\[tags-loop-continue]."
+  (interactive
+   (let ((common
+          (query-replace-read-args
+           "Query replace in marked files" nil t)))
+     (list (nth 0 common) (nth 1 common) (nth 2 common))))
+  (require 'dired-aux)
+  (dolist (file (dired-get-marked-files nil nil 'dired-nondirectory-p))
+    (let ((buffer (get-file-buffer file)))
+      (if (and buffer (with-current-buffer buffer
+                        buffer-read-only))
+          (error "File `%s' is visited read-only" file))))
+  (my-tags-query-replace
+   from to delimited '(dired-get-marked-files nil nil 'dired-nondirectory-p)))
+
+(defun my-tags-query-replace (from to &optional delimited file-list-form)
+  "Do `query-replace' of FROM with TO on all files listed in tags table.
+Third arg DELIMITED (prefix arg) means replace only word-delimited matches.
+If you exit (\\[keyboard-quit], RET or q), you can resume the query replace
+with the command \\[tags-loop-continue].
+Fourth arg FILE-LIST-FORM non-nil means initialize the replacement loop.
+Fifth and sixth arguments START and END are accepted, for compatibility
+with `query-replace', and ignored.
+
+If FILE-LIST-FORM is non-nil, it is a form to evaluate to
+produce the list of files to search.
+
+See also the documentation of the variable `tags-file-name'."
+  (interactive (query-replace-read-args "Tags query replace" nil t))
+  (require 'etags)
+  (setq tags-loop-scan `(let ,(unless (equal from (downcase from))
+                                '((case-fold-search nil)))
+                          (if (search-forward ',from nil t)
+                              ;; When we find a match, move back
+                              ;; to the beginning of it so perform-replace
+                              ;; will see it.
+                              (goto-char (match-beginning 0))))
+        tags-loop-operate `(perform-replace ',from ',to t nil ',delimited
+                                            nil multi-query-replace-map))
+  (tags-loop-continue (or file-list-form t)))
+
 ;; Toggle between a vertical and horizontal window split
 (defun toggle-window-split ()
   (interactive)
