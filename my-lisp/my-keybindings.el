446b4e9daf96f6371ade0c8fce4f091a50051089
Phil <phil@xerxes.(none)>
2013-06-11 21:47:30 +1200

    Additional buffer/file diff bindings.

--- my-lisp/my-keybindings.el
+++ my-lisp/my-keybindings.el
@@ -116,8 +116,11 @@
   (define-key keymap (kbd "C-c w a")   'my-align-next-window)
   (define-key keymap (kbd "C-x 2")     'my-split-window-below)
 
-  ;; compare-windows
+  ;; Diff / Comparison
   (define-key keymap (kbd "C-M-=")     'compare-windows)
+  (define-key keymap (kbd "C-z =")     'diff-current-buffer-with-disk)
+  (define-key keymap (kbd "C-z C-=")   'ediff-current-file)
+  (define-key keymap (kbd "C-x v C-=") 'vc-ediff)
 
   ;; Completions
   (define-key keymap (kbd "C-c /")     'my-ido-hippie-expand)
