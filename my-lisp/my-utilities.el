82b6b9b929ab78d52d1cc512cd93aaf4122385d6
Phil Sainty <phil@catalyst.net.nz>
2015-09-29 11:43:25 +1300

    fixup! Always open man pages in the same window.

--- my-lisp/my-utilities.el
+++ my-lisp/my-utilities.el
@@ -1220,6 +1220,56 @@ Will work in org-mode or in any mode that accepts plain html."
                             "</kbd><kbd>")))
       (insert (format output ""))
       (forward-char (if is-org-mode -8 -6)))))
+
+(defun display-buffer-reuse-major-mode-window (buffer alist)
+  "Return a window displaying a buffer in BUFFER's major mode.
+Return nil if no usable window is found.
+
+If ALIST has a non-nil `inhibit-same-window' entry, the selected
+window is not eligible for reuse.
+
+If ALIST contains a `reusable-frames' entry, its value determines
+which frames to search for a reusable window:
+  nil -- the selected frame (actually the last non-minibuffer frame)
+  A frame   -- just that frame
+  `visible' -- all visible frames
+  0   -- all frames on the current terminal
+  t   -- all frames.
+
+If ALIST contains no `reusable-frames' entry, search just the
+selected frame if `display-buffer-reuse-frames' and
+`pop-up-frames' are both nil; search all frames on the current
+terminal if either of those variables is non-nil.
+
+If ALIST has a non-nil `inhibit-switch-frame' entry, then in the
+event that a window on another frame is chosen, avoid raising
+that frame."
+  (let* ((alist-entry (assq 'reusable-frames alist))
+         (frames (cond (alist-entry (cdr alist-entry))
+                       ((if (eq pop-up-frames 'graphic-only)
+                            (display-graphic-p)
+                          pop-up-frames)
+                        0)
+                       (display-buffer-reuse-frames 0)
+                       (t (last-nonminibuffer-frame))))
+         (window (let ((mode (with-current-buffer buffer major-mode)))
+                   (if (and (eq mode (with-current-buffer (window-buffer)
+                                       major-mode))
+                            (not (cdr (assq 'inhibit-same-window alist))))
+                       (selected-window)
+                     (catch 'window
+                       (walk-windows
+                        (lambda (w)
+                          (and (window-live-p w)
+                               (eq mode (with-current-buffer (window-buffer w)
+                                          major-mode))
+                               (not (eq w (selected-window)))
+                               (throw 'window w)))
+                        'nomini frames))))))
+    (when (window-live-p window)
+      (prog1 (window--display-buffer buffer window 'reuse alist)
+        (unless (cdr (assq 'inhibit-switch-frame alist))
+          (window--maybe-raise-frame (window-frame window)))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
