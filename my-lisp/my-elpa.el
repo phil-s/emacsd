360dcd2ffb2749f7623b18cd01fd8dd67bde45eb
Phil Sainty <phil@catalyst.net.nz>
2015-10-20 10:01:24 +1300

    Added melpa-stable to package archive list

--- my-lisp/my-elpa.el
+++ my-lisp/my-elpa.el
@@ -16,8 +16,10 @@
 
 ;; Marmalade repository
 (require 'package)
-(add-to-list 'package-archives 
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
 ;;; Initialise:
 (unless (functionp 'package-initialize)
