cc05ba71b9eeb9ffb85f6eb09c42490b92e117fe
Phil Sainty <phil@catalyst.net.nz>
2017-02-02 18:05:39 +1300

    Remove seemingly-defunct 'marmalade' ELPA package archive

--- my-lisp/my-elpa.el
+++ my-lisp/my-elpa.el
@@ -18,8 +18,8 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+;; (add-to-list 'package-archives
+;;              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
 ;;; Initialise:
 (unless (functionp 'package-initialize)
