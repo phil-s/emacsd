d920b693607ddfa44ce0ccd08edfa45a4c038d84
Phil Sainty <phil@catalyst.net.nz>
2019-12-10 10:30:24 +1300

    Increase the default undo limits
    This doubles the values set in Emacs 27.1

--- my-lisp/my-configuration.el
+++ my-lisp/my-configuration.el
@@ -655,10 +655,12 @@ See also: `my-copy-buffer-file-name'."
       uniquify-ignore-buffers-re   "^\\*") ; don't muck with special buffers
 
 ;; Increase the default undo limits
-(when (< undo-limit 160000)
-  (setq undo-limit 160000))
-(when (< undo-strong-limit 240000)
-  (setq undo-strong-limit 240000))
+(when (< undo-limit 320000)
+  (setq undo-limit 320000))
+(when (< undo-strong-limit 480000)
+  (setq undo-strong-limit 480000))
+(when (< undo-outer-limit 48000000)
+  (setq undo-outer-limit 48000000))
 
 ;; Make large files read-only, and disable undo for the buffer
 (defun my-find-file-check-make-large-file-read-only-hook ()
