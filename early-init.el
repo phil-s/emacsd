572aef2a80330e0c14c9eb937f5f9ed1d32c8d64
Phil Sainty <phil@catalyst.net.nz>
2023-10-08 01:40:13 +1300

    Make the tab bar icons scale to the height of the tab bar

--- early-init.el
+++ early-init.el
@@ -21,3 +21,33 @@
 (my-frame-behaviours)
 ;; ...and later, for new frames / emacsclient
 (add-hook 'after-make-frame-functions 'my-frame-behaviours)
+
+;; Make the tab bar icons scale to the height of the tab bar.
+;; The magic sauce is the introduction of :height (1.0 . em)
+;; which is fixed upstream for Emacs 30 as part of bug#62562.
+;; To refresh icons after making changes to these, eval:
+;; (tab-bar--load-buttons)
+(when (< emacs-major-version 30)
+  (require 'icons)
+  (define-icon tab-bar-new nil
+    `((image "tabs/new.xpm"
+             :height (1.0 . em)
+             :margin ,tab-bar-button-margin
+             :ascent center)
+      ;; (emoji "➕")
+      ;; (symbol "＋")
+      (text " + "))
+    "Icon for creating a new tab."
+    :version "29.1"
+    :help-echo "New tab")
+  (define-icon tab-bar-close nil
+    `((image "tabs/close.xpm"
+             :height (1.0 . em)
+             :margin ,tab-bar-button-margin
+             :ascent center)
+      ;; (emoji " ❌")
+      ;; (symbol "✕") ;; "ⓧ"
+      (text " x"))
+    "Icon for closing the clicked tab."
+    :version "29.1"
+    :help-echo "Click to close tab"))
