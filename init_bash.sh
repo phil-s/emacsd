dda278bf5adb67a478081469f728c694ff788057
Phil Sainty <phil@catalyst.net.nz>
2024-11-18 16:08:31 +1300

    Set (and document) ansi colour escape codes in prompt for M-x shell
    Refer to (info "(emacs)Interactive Shell") re: init_SHELLNAME.sh

--- init_bash.sh
+++ init_bash.sh
@@ -1,5 +1,1 @@
-# better shell colours for my emacs colour scheme
-#export PS1="\n\[\e[32m\]\u@\h \[\e[34m\]\w\[\e[0m\]\n\$ "
-
-# with Zenburn (far from ideal, but better than the above)
-export PS1="\n\[\e[33m\]\u@\h \[\e[36m\]\w\[\e[0m\]\n\$ "
+PS1="\n\[\e[33m\]\u@\h: \d, \t\n\[\e[36m\]\w\[\e[0m\]\n\$ " # see init_bash.sh.notes
