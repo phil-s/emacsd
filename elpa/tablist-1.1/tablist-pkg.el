a81ed68ba414d2d5cee354953e480631952a4e60
Phil Sainty <phil@catalyst.net.nz>
2025-01-06 21:47:17 +1300

    Update tablist package to 1.1 (from 1.0)

---
+++ elpa/tablist-1.1/tablist-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "tablist" "1.1"
+  "Extended tabulated-list-mode."
+  '((emacs "24.3"))
+  :url "https://github.com/emacsorphanage/tablist"
+  :commit "5f7b71a92bfb25418d7da86ad9c45f14b149496f"
+  :revdesc "5f7b71a92bfb"
+  :keywords '("extensions" "lisp")
+  :authors '(("Andreas Politz" . "politza@fh-trier.de"))
+  :maintainers '(("Andreas Politz" . "politza@fh-trier.de")))
