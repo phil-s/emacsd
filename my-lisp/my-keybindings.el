ce808b6ab994a684bf80a89a32dfd62728abcdee
Phil Sainty <phil@catalyst.net.nz>
2025-01-06 20:49:12 +1300

    Revert "Specify symbol for :keymap in `my-keys-local-minor-mode'"
    This reverts commit de99e6aaa03b53ee5f9cdcce592c2b033c993f31.
    
    I had this reverted but uncommitted.  I don't remember why, but let's
    commit it for now.

--- my-lisp/my-keybindings.el
+++ my-lisp/my-keybindings.el
@@ -368,7 +368,7 @@ does not run, and therefore no globalized modes take effect).
 
 \\{my-keys-local-minor-mode-map}"
   :init-value t
-  :keymap 'my-keys-local-minor-mode-map)
+  :keymap my-keys-local-minor-mode-map)
 
 ;; Also make it possible to disable my keys.
 (define-globalized-minor-mode my-global-keys-local-minor-mode
