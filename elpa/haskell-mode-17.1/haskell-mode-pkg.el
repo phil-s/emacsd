561f51f50fb6214990753e1e5300099427d1d685
Phil Sainty <phil@catalyst.net.nz>
2025-01-07 11:10:31 +1300

    Update haskell-mode package to 17.5 (from 17.1)

--- elpa/haskell-mode-17.1/haskell-mode-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "haskell-mode" "17.1" "A Haskell editing mode"
-  '((emacs "25.1"))
-  :commit "17357211a16394884a981fe0c062f94250b40300" :keywords
-  '("haskell" "cabal" "ghc" "repl")
-  :url "https://github.com/haskell/haskell-mode")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
