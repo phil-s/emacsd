8cf07356b8ecb69fb2f769152b81f7357d56180d
Phil Sainty <phil@catalyst.net.nz>
2025-06-27 16:50:28 +1200

    Update pdf-tools package to version 1.1.0 (from 1.0)

---
+++ elpa/pdf-tools-1.1.0/pdf-tools-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "pdf-tools" "1.1.0"
+  "Support library for PDF documents."
+  '((emacs     "26.3")
+    (tablist   "1.0")
+    (let-alist "1.0.4"))
+  :url "http://github.com/vedang/pdf-tools/"
+  :commit "a9c9a12c3ecf2005fa641059368ac8284f507620"
+  :revdesc "v1.1.0-0-ga9c9a12c3ecf"
+  :keywords '("files" "multimedia")
+  :authors '(("Andreas Politz" . "mail@andreas-politz.de"))
+  :maintainers '(("Vedang Manerikar" . "vedang.manerikar@gmail.com")))
