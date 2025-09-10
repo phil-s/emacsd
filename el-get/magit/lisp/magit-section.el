7b7352523b9586c40568f0c999b8ea6771240791
Phil Sainty <phil@catalyst.net.nz>
2025-09-10 13:43:14 +1200

    Update Magit to version 4.4.0 (from 4.4.0-pre)

--- el-get/magit/lisp/magit-section.el
+++ el-get/magit/lisp/magit-section.el
@@ -8,13 +8,13 @@
 ;; Homepage: https://github.com/magit/magit
 ;; Keywords: tools
 
-;; Package-Version: 4.3.8
+;; Package-Version: 4.4.0
 ;; Package-Requires: (
-;;     (emacs "28.1")
-;;     (compat "30.1")
+;;     (emacs   "28.1")
+;;     (compat  "30.1")
 ;;     (cond-let "0.1")
-;;     (llama "1.0")
-;;     (seq "2.24"))
+;;     (llama    "1.0")
+;;     (seq      "2.24"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
