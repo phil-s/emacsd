2b42fdf7d7ea8b66aeaeca2ab765c7ddbd5e1e5c
Phil Sainty <phil@catalyst.net.nz>
2025-09-17 16:38:42 +1200

    Update web-mode library to version 17.3.22 (from 12.4.2)

--- el-get/.loaddefs.el
+++ el-get/.loaddefs.el
@@ -6165,12 +6165,26 @@ it is disabled.
 ;;;;;;  (0 0 0 0))
 ;;; Generated autoloads from web-mode/web-mode.el
 
+(put 'web-mode-attr-indent-offset 'safe-local-variable #'(lambda (v) (or (integerp v) (booleanp v))))
+
+(put 'web-mode-attr-value-indent-offset 'safe-local-variable #'(lambda (v) (or (integerp v) (booleanp v))))
+
+(put 'web-mode-markup-indent-offset 'safe-local-variable #'integerp)
+
+(put 'web-mode-markup-comment-indent-offset 'safe-local-variable #'integerp)
+
+(put 'web-mode-css-indent-offset 'safe-local-variable #'integerp)
+
+(put 'web-mode-code-indent-offset 'safe-local-variable #'integerp)
+
+(put 'web-mode-sql-indent-offset 'safe-local-variable #'integerp)
+
 (autoload 'web-mode "web-mode/web-mode" "\
 Major mode for editing web templates.
 
-\(fn)" t nil)
+\(fn)" t)
 
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "web-mode/web-mode" '("web-mode-")))
+(register-definition-prefixes "web-mode/web-mode" '("web-mode-"))
 
 ;;;***
 
